019a48f81bb4b67a3c43c00c61a5f7fc428f37b7
YunaSong97 <unme97@naver.com>
2022-05-25 21:30:43 +0900

    popup slideup

---
+++ app/src/main/java/com/example/im_geokjeong/MapDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.im_geokjeong
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_detail)
+    }
+}
