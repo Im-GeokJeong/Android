e1f44c3462d6038b5f9b778a9ef6224af3f548e1
leehandsub <gustjq7027@naver.com>
2022-05-30 17:45:55 +0900

    개인 임대 ui만들기
    개인 임대 화면
    상세화면 만들기

--- app/src/main/java/com/example/im_geokjeong/ui/rentalperson/RentalPersonFragment.kt
+++ app/src/main/java/com/example/im_geokjeong/ui/rentalperson/RentalPersonFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.im_geokjeong.R
 
@@ -14,5 +15,12 @@ class RentalPersonFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_rental_person, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        val btn = view.findViewById<TextView>(R.id.btn_hello)
+//        btn.setOnClickListener {
+//
+//        }
     }
 }
